69dc117d75bce7c0825149957c6d29160d08b9c3
feelgoodincorp <newuser96@ukr.net>
2023-12-10 13:29:47 +0200

    feature: init state machine with mvc sources without refactoring

---
+++ app/src/main/java/com/app/user/demo/MainViewState.java
@@ -0,0 +1,19 @@
+package com.app.user.demo;
+
+abstract class MainViewState {
+
+    void onEnter(Contract.MenuView view){}
+    void setOwner(Contract.MenuPresenter owner){}
+
+    //could require view as parameter
+    void onExit(){}
+    void restoreState(Contract.MenuView view){}
+
+    int [] generateAnimatingQueue(int pivot, int arraySize){
+        int sortedArray [] = new int [arraySize];
+        for(int a = 0; a<arraySize; a++){
+            sortedArray[a] = a>pivot ? (a-pivot) : (pivot-a);
+        }
+        return sortedArray;
+    }
+}
